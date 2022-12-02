d7fa7aeddfc7c62851dd30492e6556ff2dff3b2b
SrBolaa <2gabrie1996@gmail.com>
2022-12-02 16:09:57 -0300

    arrumando carrossel

---
+++ Js/index.js
@@ -0,0 +1,10 @@
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".items");
+const maxItems = items.length;
+
+controls.forEach(control => {  
+    control.addEventListener("click", () => {
+        console.log("control cliked");
+    })
+});
