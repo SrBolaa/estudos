f3fcdd1c4479b7b02db7fea55f4eda45f0ecd481
SrBolaa <2gabrie1996@gmail.com>
2022-12-02 17:04:41 -0300

    arrumado problema do botão não funcionar

--- Js/index.js
+++ Js/index.js
@@ -1,10 +1,36 @@
 const controls = document.querySelectorAll(".control");
 let currentItem = 0;
-const items = document.querySelectorAll(".items");
+const items = document.querySelectorAll(".linha-vertical");
 const maxItems = items.length;
 
-controls.forEach(control => {  
-    control.addEventListener("click", () => {
-        console.log("control cliked");
-    })
+controls.forEach((control) => {
+  control.addEventListener("click", (e) => {
+    console.log("DSADSA")
+    isLeft = e.target.classList.contains("arrow-left");
+
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    if (currentItem >= maxItems) {
+      currentItem = 0;
+    }
+
+    if (currentItem < 0) {
+      currentItem = maxItems - 1;
+    }
+
+    items.forEach((item) => item.classList.remove("current-item"));
+
+    items[currentItem].scrollIntoView({
+      behavior: "smooth",
+      inline: "center"
+    });
+
+    items[currentItem].classList.add("current-item");
+  });
 });
+
+
